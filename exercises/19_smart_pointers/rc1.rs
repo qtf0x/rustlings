9e1e64845f06d1fafb0bf59e4f9724757d4c552d
qtf0x <qtf0x@pm.me>
2026-01-21 13:57:06 -0700

    completed smart pointers exercises

--- exercises/19_smart_pointers/rc1.rs
+++ exercises/19_smart_pointers/rc1.rs
@@ -59,18 +59,18 @@ mod tests {
         println!("reference count = {}", Rc::strong_count(&sun)); // 6 references
         jupiter.details();
 
-        // TODO
-        let saturn = Planet::Saturn(Rc::new(Sun));
+        // DONE
+        let saturn = Planet::Saturn(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 7 references
         saturn.details();
 
-        // TODO
-        let uranus = Planet::Uranus(Rc::new(Sun));
+        // DONE
+        let uranus = Planet::Uranus(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 8 references
         uranus.details();
 
-        // TODO
-        let neptune = Planet::Neptune(Rc::new(Sun));
+        // DONE
+        let neptune = Planet::Neptune(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 9 references
         neptune.details();
 
@@ -91,13 +91,16 @@ mod tests {
         drop(mars);
         println!("reference count = {}", Rc::strong_count(&sun)); // 4 references
 
-        // TODO
+        // DONE
+        drop(earth);
         println!("reference count = {}", Rc::strong_count(&sun)); // 3 references
 
-        // TODO
+        // DONE
+        drop(venus);
         println!("reference count = {}", Rc::strong_count(&sun)); // 2 references
 
-        // TODO
+        // DONE
+        drop(mercury);
         println!("reference count = {}", Rc::strong_count(&sun)); // 1 reference
 
         assert_eq!(Rc::strong_count(&sun), 1);
