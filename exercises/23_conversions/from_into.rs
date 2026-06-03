3915c666bf37e43fed57f9bf10d75d7c808995bb
qtf0x <qtf0x@pm.me>
2026-06-03 00:12:51 -0600

    completed conversions exercises

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -20,7 +20,7 @@ impl Default for Person {
     }
 }
 
-// TODO: Complete this `From` implementation to be able to parse a `Person`
+// DONE: Complete this `From` implementation to be able to parse a `Person`
 // out of a string in the form of "Mark,20".
 // Note that you'll need to parse the age component into a `u8` with something
 // like `"4".parse::<u8>()`.
@@ -34,7 +34,23 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let [name, age] = s.split(',').collect::<Vec<_>>()[..] else {
+            return Self::default();
+        };
+
+        let name = if name.is_empty() {
+            return Self::default();
+        } else {
+            name.into()
+        };
+
+        let Ok(age) = age.parse() else {
+            return Self::default();
+        };
+
+        Self { name, age }
+    }
 }
 
 fn main() {
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let [name, age] = s.split(',').collect::<Vec<_>>()[..] else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        let name = if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        } else {
+            name.into()
+        };
+
+        let age = age.parse().map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Self { name, age })
+    }
 }
 
 fn main() {
